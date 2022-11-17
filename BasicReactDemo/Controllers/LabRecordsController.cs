8c96577f0f1247e9f84f82339434962d5475125f
crjcodes <crjcodes@users.noreply.github.com>
2022-11-17 04:48:16 -0600

    Initial commit

---
+++ BasicReactDemo/Controllers/LabRecordsController.cs
@@ -0,0 +1,83 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BasicReactDemo.Controllers
+{
+    public class LabRecordsController : Controller
+    {
+        // GET: LabRecordsController
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: LabRecordsController/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: LabRecordsController/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: LabRecordsController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: LabRecordsController/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: LabRecordsController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: LabRecordsController/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: LabRecordsController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ BasicReactDemo/Models/LabRecord.cs
@@ -0,0 +1,11 @@
+﻿namespace BasicReactDemo.Model
+{
+    public class LabRecord
+    {
+        public string Name { get; set; } = string.Empty;
+        //public string Category { get; set; }
+        //public string Subcategory { get; set; }
+
+        public List<LabMeasurement> Measurements { get; set; } = new List<LabMeasurement>();
+    }
+}
